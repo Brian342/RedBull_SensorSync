0bfd283e64009369ccc857ec1756017a9dd42f06
Brian kimanzi <migelbrian3@gmail.com>
2026-03-06 00:51:40 +0300

    new code update

--- Sensor_Data.py
+++ Sensor_Data.py
@@ -2,4 +2,39 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from SensorSync_FrontPage import *
 
+# function for checking the null values
+def fillnull(df: pd.DataFrame ,col: str) -> str:
+    print(f"Print the no of null values: {df.isna().sum()}")
+    print("=======================================================================")
+    print()
+    print(f" {df[df[col].isna()]}")
+    print()
+    print("========================================================================")
+    print(f"fill the null values using forward fill: {df[col].ffill(inplace=True)}")
+    print("========================================================================")
+    print()
+    print(f"fill the null values using backward fill: {df[col].bfill(inplace=True)}")
+    print()
+    print("=======================================================================")
+    print(f"Print the no of null values: {df.isna().sum()}")
+    print()
+
+if data_csv is not None:
+    fillnull(data_csv)
+elif data_excel is not None:
+    fillnull(data_excel)
+else:
+    print("No file uploaded")
+
+
+if data_csv is not None:
+    combine_copy = data_csv.copy()
+    combine_copy['age'].fillna(combine_copy['age'].median(),inplace=True)
+
+elif data_excel is not None:
+    combine_copy = data_excel.copy()
+    combine_copy['age'].fillna(combine_copy['age'].median(),inplace=True)
+
+# manipulate the date time column
