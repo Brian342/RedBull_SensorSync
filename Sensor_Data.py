7b3728e016b44ebf80e2123f2e60e8f3498707d1
Brian kimanzi <migelbrian3@gmail.com>
2026-03-11 08:15:50 +0300

    new code update

--- Sensor_Data.py
+++ Sensor_Data.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # function for checking the null values
-def cleaning(df: pd.DataFrame, col:str) -> str:
+def cleaning(data_csv, data_excel: pd.DataFrame, col:str) -> str:
     if data_csv is not None:
         print(f"Print the no of null values: {data_csv.isna().sum()}")
         print("=======================================================================")
@@ -38,34 +38,33 @@ def cleaning(df: pd.DataFrame, col:str) -> str:
         print()
 
 
+    if data_csv is not None:
+        combine_copy = data_csv.copy()
+        combine_copy['age'].fillna(combine_copy['age'].median(),inplace=True)
 
-
-if data_csv is not None:
-    combine_copy = data_csv.copy()
-    combine_copy['age'].fillna(combine_copy['age'].median(),inplace=True)
-
-elif data_excel is not None:
-    combine_copy = data_excel.copy()
-    combine_copy['age'].fillna(combine_copy['age'].median(),inplace=True)
+    elif data_excel is not None:
+        combine_copy = data_excel.copy()
+        combine_copy['age'].fillna(combine_copy['age'].median(),inplace=True)
 
 # manipulate the date time column
+
 def manipulate_date(df: pd.DataFrame):
-    combine_copy['datetime']= pd.to_datetime(combine_copy['datetime'])
+    df['datetime']= pd.to_datetime(df['datetime'])
 
 # separating the the datetime column into year, month, date and hour
-    combine_copy['year'] = combine_copy['datetime'].dt.year
-    combine_copy['month'] = combine_copy['datetime'].dt.month
-    combine_copy['date'] = combine_copy['datetime'].dt.day
-    combine_copy['hour'] = combine_copy['datetime'].dt.hour
+    df['year'] = df['datetime'].dt.year
+    df['month'] = df['datetime'].dt.month
+    df['date'] = df['datetime'].dt.day
+    df['hour'] = df['datetime'].dt.hour
 
 # using dictionary
     month = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun',
             7:'July', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
 
-    combine_copy['month'] = combine_copy['month'].map(month)
+    df['month'] = df['month'].map(month)
 
 # change the age column to int type
-    combine_copy['age'] = combine_copy['age'].astype(int)
+    df['age'] = df['age'].astype(int)
 
 
 def round_value(df: pd.DataFrame, col: float) -> float:
@@ -73,5 +72,5 @@ def round_value(df: pd.DataFrame, col: float) -> float:
 
 values = ['volt','rotate', 'pressure', 'vibration']
 for i in values:
-    round_value(combine_copy, i)
+    round_value(df, i)
      
